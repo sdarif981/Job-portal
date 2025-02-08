a3ddca7ffcba8cb9bc05334a0bd190d9a7d566ac
Syed Arif <sdarif981@gmail.com>
2025-02-08 19:28:40 +0530

    Added new files

---
+++ Frontend/src/components/FilterCard.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import { RadioGroup, RadioGroupItem } from './ui/radio-group';
+import { Label } from './ui/label';
+import { useDispatch } from 'react-redux';
+import { setSearchedQuery } from '@/redux/jobSlice';
+
+const filterData = [
+    {
+        filterType: "All Jobs",
+        array: ["All Jobs"]
+    },
+    {
+        filterType: "Location",
+        array: ["Delhi NCR", "Bangalore", "Hyderabad", "Pune", "Mumbai"]
+    },
+    {
+        filterType: "Industry",
+        array: ["Frontend Developer", "Backend Developer", "FullStack Developer"]
+    },
+    {
+        filterType: "Work Type",
+        array: ["Remote", "Work From Home", "Hybrid", "Full-Time"] 
+    },
+    
+];
+
+const FilterCard = () => {
+    const [selectedValue, setSelectedValue] = useState('All Jobs'); // Default to "All Jobs"
+    const dispatch = useDispatch();
+
+    const changeHandler = (value) => {
+        setSelectedValue(value);
+    };
+
+    useEffect(() => {
+        dispatch(setSearchedQuery(selectedValue === "All Jobs" ? "" : selectedValue));
+    }, [selectedValue, dispatch]);
+
+    return (
+        <div className='w-full bg-white p-3 rounded-md'>
+            <h1 className='font-bold text-lg'>Filter Jobs</h1>
+            <hr className='mt-3' />
+            <RadioGroup value={selectedValue} onValueChange={changeHandler}>
+                {filterData.map((data, index) => (
+                    <div key={index}>
+                        <h1 className='font-bold text-lg'>{data.filterType}</h1>
+                        {data.array.map((item, idx) => {
+                            const itemId = `id${index}-${idx}`;
+                            return (
+                                <div className='flex items-center space-x-2 my-2' key={idx}>
+                                    <RadioGroupItem value={item} id={itemId} />
+                                    <Label htmlFor={itemId}>{item}</Label>
+                                </div>
+                            );
+                        })}
+                    </div>
+                ))}
+            </RadioGroup>
+        </div>
+    );
+};
+
+export default FilterCard;
