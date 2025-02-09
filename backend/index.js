f8ee1bd823a6f1724049439a07b3bc68e14195fd
Syed Arif <sdarif981@gmail.com>
2025-02-09 16:04:49 +0530

    added files

---
+++ backend/index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.routes.js";
+import companyRoute from "./routes/company.routes.js"
+import jobRoute from "./routes/job.route.js"
+import applicationRoute from "./routes/application.route.js"
+import path from 'path';
+dotenv.config();
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true
+};
+app.use(cors(corsOptions));
+
+const port = process.env.PORT || 3000;
+
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job",jobRoute);
+app.use("/api/v1/application",applicationRoute);
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+
+// Error Handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
