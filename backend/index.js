bd6cfb8d41b7c70d35a31b7bdab8e433bde320f5
Syed Arif <sdarif981@gmail.com>
2025-02-09 16:40:55 +0530

    added new changes

--- backend/index.js
+++ backend/index.js
@@ -4,22 +4,22 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.routes.js";
-import companyRoute from "./routes/company.routes.js"
-import jobRoute from "./routes/job.route.js"
-import applicationRoute from "./routes/application.route.js"
-import path from 'path';
+import companyRoute from "./routes/company.routes.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+import path from "path";
 dotenv.config();
 
 const app = express();
-const _dirname=path.resolve();
+const _dirname = path.resolve();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
-  credentials: true
+  origin: "http://localhost:5173",
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
@@ -27,21 +27,21 @@ const port = process.env.PORT || 3000;
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/job",jobRoute);
-app.use("/api/v1/application",applicationRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 app.listen(port, () => {
   connectDB();
   console.log(`Server listening at http://localhost:${port}`);
 });
 
-app.use(express.static(path.join(_dirname,"/frontend/dist")));
-app.get('*',(_,res)=>{
-  res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
-})
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
 
 // Error Handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send("Something broke!");
 });
