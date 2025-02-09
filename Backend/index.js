3d8b35f3a0340545c6664098019e651427dff8b3
Syed Arif <sdarif981@gmail.com>
2025-02-09 14:39:19 +0530

    Ready Application

--- Backend/index.js
+++ Backend/index.js
@@ -7,11 +7,11 @@ import userRoute from "./routes/user.routes.js";
 import companyRoute from "./routes/company.routes.js"
 import jobRoute from "./routes/job.route.js"
 import applicationRoute from "./routes/application.route.js"
-
+import path from 'path';
 dotenv.config();
 
 const app = express();
-
+const _dirname=path.resolve();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -29,7 +29,10 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job",jobRoute);
 app.use("/api/v1/application",applicationRoute);
-
+app.use(express.static(path.join(_dirname,"/frontend/dist")));
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+});
 app.listen(port, () => {
   connectDB();
   console.log(`Server listening at http://localhost:${port}`);
